360dcb16cda7e33f692b06e5c06a650a0283895c
aristophanesyates <56827855+aristophanesyates@users.noreply.github.com>
2020-08-19 18:37:34 +0100

    Made very poorly implemented boob physics (pendulum method)

---
+++ Assets/BoobTether.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoobTether : MonoBehaviour
+{
+    [SerializeField] private GameObject boob;
+    private Rigidbody boobRB;
+    [SerializeField] float tetherLength = 0.25f;
+    private bool once = false;
+    private float distance
+    {
+        get
+        {
+            return (boob.transform.position - transform.position).magnitude;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        boobRB = boob.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (distance > tetherLength && !once)
+        {
+            boobRB.velocity += (-(transform.position - boob.transform.position).normalized * Vector3.Dot(boobRB.velocity, transform.position - boob.transform.position)) * 2;
+            Vector3 displacement = (transform.position - boob.transform.position) - (transform.position - boob.transform.position).normalized * tetherLength;
+            boobRB.velocity -= displacement * 2;
+            boob.transform.position += (transform.position - boob.transform.position) * Mathf.Abs(tetherLength - distance);
+            //once = true;
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float jumpSpeed = 10f;
+    [SerializeField] private bool jump = false;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (jump)
+        {
+            rb.velocity += Vector3.up * jumpSpeed;
+            jump = false;
+        }
+    }
+}
